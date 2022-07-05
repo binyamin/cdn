41101e9d32949115354328e7c754e34f9ad75580
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-05 18:47:09 -0400

    fix typo in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/opt/buildhome/.deno"
-export PATH="$DENO_INSTAL/bin:$PATH"
+export PATH="$DENO_INSTALL/bin:$PATH"
 deno task build
