d07b36d95cf1eb69073704e140e7c4fa68b3e679
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-05 20:39:17 -0400

    Fix netlify config, & match pkg data

--- netlify/edge-functions/pkg.ts
+++ netlify/edge-functions/pkg.ts
@@ -1,9 +1,34 @@
+import * as path from 'https://deno.land/std@0.147.0/path/mod.ts';
 import type { EdgeFunction } from 'netlify:edge';
 
+interface Module {
+	name: string;
+	version: string;
+	tag?: string;
+	path: string;
+}
+
+const pattern = new URLPattern({
+	pathname: '/pkg/:name([a-zA-Z_\\-0-9]+){@:version([a-zA-Z_\\-0-9\\.]+)}?/:path+',
+});
+
 const handler: EdgeFunction = (request, context) => {
-	return Response.json({
-		title: "hello",
-	});
+	const matches = pattern.exec(request.url);
+
+	if (!matches) return;
+
+	const module: Module = {
+		name: matches.pathname.groups.name,
+		version: matches.pathname.groups.version || 'latest',
+		path: matches.pathname.groups.path,
+	}
+
+	if (/^v?\d+\.\d+\.\d+$/.test(module.version) === false) {
+		module.tag = module.version;
+		module.version = '[replace me]';
+	}
+
+	return Response.json(module);
 }
 
 export default handler;
