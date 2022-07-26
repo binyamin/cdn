073b10c1a36dba9030405bff856cc6b206c0023a
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-26 11:45:24 -0400

    Update deps

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ import lume from "lume/mod.ts";
 import attributes from "lume/plugins/attributes.ts";
 import date from "lume/plugins/date.ts";
 import prism from "lume/plugins/prism.ts";
-import sass from "https://denopkg.dev/gl/binyamin/lume-sass@v0.1.0/mod.ts";
-import parcel_css from "https://denopkg.dev/gl/binyamin/lume-parcel-css@v0.1.1/mod.ts";
+import sass from "https://denopkg.dev/gl/binyamin/lume-sass@v0.2.0/mod.ts";
+import parcel_css from "https://denopkg.dev/gl/binyamin/lume-parcel-css@v0.1.2/mod.ts";
 
 import extras from './helpers/extras.ts';
 
