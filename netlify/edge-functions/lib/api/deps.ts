4304d3bd5f6eb290d4490c8831870b6927fbd63d
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-26 20:52:02 -0400

    Fix careless mistake

--- netlify/edge-functions/lib/api/deps.ts
+++ netlify/edge-functions/lib/api/deps.ts
@@ -8,5 +8,7 @@ const env = await config() as {
 env.GITHUB_TOKEN ??= Deno.env.get('GITHUB_TOKEN')!;
 env.GITLAB_TOKEN ??= Deno.env.get('GITLAB_TOKEN')!;
 
+export { env };
+
 import { decode } from 'https://deno.land/std@0.149.0/encoding/base64.ts';
 export const base64 = { decode };
