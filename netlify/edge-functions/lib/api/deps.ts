9bd024cdac23af2965322d3e5735f8cc134371b5
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-07-26 20:50:11 -0400

    Try again to fix netlify deployment

--- netlify/edge-functions/lib/api/deps.ts
+++ netlify/edge-functions/lib/api/deps.ts
@@ -1,8 +1,12 @@
-import { configSync } from 'https://deno.land/std@0.149.0/dotenv/mod.ts';
-export const env = configSync() as {
+import { config } from 'https://deno.land/std@0.149.0/dotenv/mod.ts';
+
+const env = await config() as {
 	GITHUB_TOKEN: string;
 	GITLAB_TOKEN: string;
 };
+
+env.GITHUB_TOKEN ??= Deno.env.get('GITHUB_TOKEN')!;
+env.GITLAB_TOKEN ??= Deno.env.get('GITLAB_TOKEN')!;
 
 import { decode } from 'https://deno.land/std@0.149.0/encoding/base64.ts';
 export const base64 = { decode };
