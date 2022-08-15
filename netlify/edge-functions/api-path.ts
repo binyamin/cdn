79c4e26b069e28c422009352ba040d9fb2efe918
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-08-15 16:00:24 -0400

    Add error handling for path-completion API

--- netlify/edge-functions/api-path.ts
+++ netlify/edge-functions/api-path.ts
@@ -4,6 +4,7 @@ import { calculate as getETag } from 'https://deno.land/x/oak@v10.6.0/etag.ts';
 import type { EdgeFunction } from 'netlify:edge';
 import type { CompletionList } from './api/types.ts';
 import { listPaths } from './lib/api/gitlab.ts';
+import { isHttpError, isServerErrorStatus, Status, STATUS_TEXT } from './lib/http.ts';
 
 const pattern = new URLPattern({
 	pathname: '/api/x/:package([a-zA-Z0-9_-]+)/:version([a-zA-Z0-9\\.+-]+)/paths/:path*',
@@ -16,40 +17,67 @@ const handler: EdgeFunction = async (request, context) => {
 
 	const groups = matches.pathname.groups;
 
-	const result = await listPaths(
-		groups.package,
-		groups.version,
-	);
+	try {
+		const result = await listPaths(
+			groups.package,
+			groups.version,
+		);
 
-	const list: CompletionList = {
-		items: result.paths.sort(),
-		isIncomplete: true,
-	};
+		const list: CompletionList = {
+			items: result.paths.sort(),
+			isIncomplete: true,
+		};
 
-	// If search-term is provided, fill the `preselect` field
-	if (groups.path) {
-		list.items = list.items.filter(p => p.startsWith(groups.path)).sort();
-		list.preselect = list.items[0];
-	}
-
-	const etag = await getETag(JSON.stringify(list));
-
-	if (request.headers.has('if-none-match')) {
-		const prevEtag = request.headers.get('if-none-match')!;
-
-		if (compareEtag(etag, prevEtag)) {
-			return new Response(null, {
-				status: 304,
-			})
+		// If search-term is provided, fill the `preselect` field
+		if (groups.path) {
+			list.items = list.items.filter(p => p.startsWith(groups.path)).sort();
+			list.preselect = list.items[0];
 		}
-	}
 
-	return context.json(list, {
-		status: 200,
-		headers: {
-			'ETag': etag,
+		const etag = await getETag(JSON.stringify(list));
+
+		if (request.headers.has('if-none-match')) {
+			const prevEtag = request.headers.get('if-none-match')!;
+
+			if (compareEtag(etag, prevEtag)) {
+				return new Response(null, {
+					status: 304,
+				})
+			}
 		}
-	})
+
+		return context.json(list, {
+			status: 200,
+			headers: {
+				'ETag': etag,
+			}
+		})
+	} catch(error) {
+		if (isHttpError(error)) {
+			const url = (new URL(request.url)).pathname;
+
+			if (isServerErrorStatus(error.status)) {
+				context.log(url);
+				context.log(error);
+			} else {
+				context.log(`${error.message} (${error.status}) - ${url}`);
+			}
+
+			if (error.expose) {
+				return context.json({
+					message: error.message,
+					code: error.status,
+				})
+			} else {
+				return context.json({
+					message: STATUS_TEXT[Status.InternalServerError],
+					code: Status.InternalServerError,
+				})
+			}
+		}
+
+		throw error;
+	}
 }
 
 export default handler;
--- netlify/edge-functions/lib/http.ts
+++ netlify/edge-functions/lib/http.ts
@@ -19,3 +19,9 @@ export {
 	createHttpError,
 	isHttpError,
 } from 'https://deno.land/std@0.149.0/http/http_errors.ts';
+
+export {
+	Status,
+	STATUS_TEXT,
+	isServerErrorStatus
+} from 'https://deno.land/std@0.152.0/http/http_status.ts'
